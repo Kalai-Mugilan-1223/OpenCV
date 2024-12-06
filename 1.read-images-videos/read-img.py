f128e66161627ca5084448c12c997deaf4545c1b
Kalai Mugilan R P <133219139+Kalai-Mugilan-1223@users.noreply.github.com>
2024-12-06 13:12:34 +0530

    Updated read-img.py

--- 1.read-images-videos/read-img.py
+++ 1.read-images-videos/read-img.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-img = cv.imread('read-images-videos\\Images\\Kadisi Ulaga por x Big Dawgs.png')
+img = cv.imread('1.read-images-videos\\images\\Kadisi Ulaga por x Big Dawgs.png')
 cv.imshow('Big-Dawgs',img)
 
 cv.waitKey(0)
