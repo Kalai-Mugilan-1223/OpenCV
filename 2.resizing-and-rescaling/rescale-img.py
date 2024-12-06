79fa0f75bd5e93ac1150837d9c14f3b380573d6e
Kalai Mugilan R P <133219139+Kalai-Mugilan-1223@users.noreply.github.com>
2024-12-06 13:15:10 +0530

    Add files via upload
    Uploaded code for rescaling Images and Videos

---
+++ 2.resizing-and-rescaling/rescale-img.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+def frame_rescale(frame,scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+
+
+img = cv.imread('read-images-videos\\Images\\Kadisi Ulaga por x Big Dawgs.png')
+cv.imshow('Big-Dawgs',img)
+
+img_resized = frame_rescale(img,0.3)
+cv.imshow('Resized_img',img_resized)
+
+cv.waitKey(0)
---
+++ 2.resizing-and-rescaling/rescale-video.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+def frame_rescale(frame,scale = 0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+
+capture  = cv.VideoCapture('D:\\code-tools\\Code\\Python\\OpenCV\\read-images-videos\\Videos\\Leo x deadpool_mixdown_1.mp4')
+
+while True:
+    isTrue,frame = capture.read()
+    cv.imshow('D&Wxleo',frame)
+    resized_frame = frame_rescale(frame,0.65)
+    cv.imshow('Resized',resized_frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
