ed552a6e82317f3b3cc6f561d17e969ad769149c
Kalai Mugilan R P <133219139+Kalai-Mugilan-1223@users.noreply.github.com>
2024-12-06 13:13:15 +0530

    Updated read-video.py

--- 1.read-images-videos/read-video.py
+++ 1.read-images-videos/read-video.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 
-capture  = cv.VideoCapture('D:\\code-tools\\Code\\Python\\OpenCV\\read-images-videos\\Videos\\Leo x deadpool_mixdown_1.mp4')
+capture  = cv.VideoCapture('1.read-images-videos\\Videos\\cat.mp4')
 
 while True:
     isTrue,frame = capture.read()
-    cv.imshow('Garudan',frame)
+    cv.imshow('cat',frame)
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
 
